5e134b11ae33d8e1d5180f856fa765b7ff8fb05e
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-09-08 14:47:46 +0300

    First commit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
+
   const name = ''
 
   function handleChange(event) {
