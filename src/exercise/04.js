df75b242fe3d85bbea44f5b822eb3337f1359e04
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-09-24 15:50:52 +0300

    Use local storage state custom hook

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,12 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
-  const [squares, setSquares] = React.useState(
-    () => JSON.parse(localStorage.getItem('squares')) ?? Array(9).fill(null),
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
   )
 
   const winner = calculateWinner(squares)
@@ -45,10 +47,6 @@ function Board() {
       </button>
     )
   }
-
-  React.useEffect(() => {
-    localStorage.setItem('squares', JSON.stringify(squares))
-  }, [squares])
 
   return (
     <div>
